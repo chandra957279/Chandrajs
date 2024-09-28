611b604f3372376e9de2e9d476d834fa47e7b784
chandra957279 <155599677+chandra957279@users.noreply.github.com>
2024-09-28 19:56:23 +0000

    01_variable commit done

--- basic/01_variable.js
+++ basic/01_variable.js
@@ -1,15 +1,15 @@
-// const accountId = 95727876;
-// let accountEmail = "hello67@gmail.com";
-// var accountPassword = "95676";
-// accountCity = "Jharkhand";
+const accountId = 95727876;
+let accountEmail = "hello67@gmail.com";
+var accountPassword = "95676";
+accountCity = "Jharkhand";
 
-// console.table([accountId, accountEmail, accountPassword, accountCity]);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
 
-function userAccunt() {
-    const accountId = 95727876;
-    let accountEmail = "hello67@gmail.com";
-    var accountPassword = "95676";
-    accountCity = "Jharkhand";
-}
-userAccunt();
+// function userAccunt() {
+//     const accountId = 95727876;
+//     let accountEmail = "hello67@gmail.com";
+//     var accountPassword = "95676";
+//     accountCity = "Jharkhand";
+// }
+// userAccunt()
 // console.log(userAccunt);
