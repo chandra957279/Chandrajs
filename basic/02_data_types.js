16dfdf218794d1e018b4982887d4e20cea861c88
chandra957279 <155599677+chandra957279@users.noreply.github.com>
2025-06-02 15:37:29 +0000

    container config code

---
+++ basic/02_data_types.js
@@ -0,0 +1,19 @@
+let age = 18;
+let isLoggedIn = false;
+let state;
+
+// number => 2 to power 53
+//bigint 
+// String => ""
+// boolean => true / false
+// null => standalone value
+// undefines => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof"string");
+console.log(typeof age);
+console.log(typeof undefined);
+
