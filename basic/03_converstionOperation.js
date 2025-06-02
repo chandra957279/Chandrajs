52c4b4ff69800fd99cb746e6c66f88632558838f
chandra957279 <155599677+chandra957279@users.noreply.github.com>
2025-06-02 16:11:04 +0000

    Operation

--- basic/03_converstionOperation.js
+++ basic/03_converstionOperation.js
@@ -23,4 +23,26 @@ let stirngNumber =String(someNUmber);
 console.log(stirngNumber);
 console.log(typeof stirngNumber);
 
+// ******************** Operations *******************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+ 
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+
+
+let str1 = "hello"
+let str2 = " World!"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
 
