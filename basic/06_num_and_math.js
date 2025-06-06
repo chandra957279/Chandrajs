cef6bb0983d3faea22aecda66539dc045bf31b13
chandra957279 <155599677+chandra957279@users.noreply.github.com>
2025-06-06 08:13:28 +0000

    commit-math-and -num

---
+++ basic/06_num_and_math.js
@@ -0,0 +1,41 @@
+const score = 400
+// console.log(score);
+
+
+const balance = new Number(100)
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const OtherNumber = 123.8966
+
+// console.log(OtherNumber.toPrecision(4)); // 
+
+const hundred = 1000000
+// console.log(hundred.toLocaleString('en-IN')); 
+
+
+// *****************************MATHS*******************************
+
+// console.log(Math);
+
+// console.log(Math.abs(4));
+
+// console.log(Math.round(4.5)); 
+// console.log(Math.ceil(4.1)); // 5
+// console.log(Math.floor(4.9)); // 4
+// console.log(Math.min(4, 5, 6, 7, 8)); // 4
+// console.log(Math.max(4, 5, 6, 7, 8)); // 8
+console.log(Math.random()); // 0 to 1
+console.log(Math.floor(Math.random() * 10) + 1); // 0 t
+
+
+const min = 10
+const max = 20
+
+const newMath= Math.random() * (max - min + 1) + min; // 10 to 20
+console.log(Math.floor(newMath)); // 10 to 20
+
+
