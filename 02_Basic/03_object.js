0ea0eb3953a44e06d320565871efea49ca8a8ac8
chandra957279 <155599677+chandra957279@users.noreply.github.com>
2025-06-06 10:51:45 +0000

    commit object Literals

---
+++ 02_Basic/03_object.js
@@ -0,0 +1,9 @@
+// Object Literals
+
+const JsUser = {
+    name: "Tuntun",
+    age: 22,
+    location: "Noida"
+}
+console.log(JsUser.name);
+console.log(`My name is ${JsUser.name} and my age is ${JsUser.age} i'm from ${JsUser.location}`);
